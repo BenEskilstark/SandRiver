948790c237edc134821f6e467090c55874017f8d
beskilstark <ben.eskilstark@gmail.com>
2025-04-03 10:44:00 -0400

    Add postVisit

---
+++ js/postVisit.js
@@ -0,0 +1,16 @@
+
+const URL = 'https://benhub.io/analytics/visit';
+
+const postVisit = (path, map) => {
+    map = map ?? "GET";
+    const isUnique = !!!localStorage.getItem('isRevisit');
+    localStorage.setItem('isRevisit', true);
+    fetch(URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ hostname: window.location.hostname, path, isUnique, map, })
+    })
+}
+
+window.postVisit = postVisit;
+export { postVisit }
